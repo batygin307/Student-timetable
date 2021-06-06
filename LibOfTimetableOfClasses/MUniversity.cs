43e89d7b8753362e72eea205cde3115f1d96ea8b
batygin307 <79691517+batygin307@users.noreply.github.com>
2021-06-06 12:48:17 +0300

    Update MUniversity.cs
    #1500 - Fix 4 Style, 1 Duplication issues in multiple files. Удалены пустые строки и метод isLetterСaseNormal перенесен в класс LetterCaseNormal.

--- LibOfTimetableOfClasses/MUniversity.cs
+++ LibOfTimetableOfClasses/MUniversity.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace LibOfTimetableOfClasses
+namespace LibOfTimetableOfClasses : LetterCaseNormal
 {
-
-
     /// <summary>
     /// ВУЗ
     /// </summary>
@@ -290,31 +288,6 @@ namespace LibOfTimetableOfClasses
             MiddleNameRector = middleNameRector;
             Email = email;
             Phone = phone;
-
-        }
-
-        private bool isLetterСaseNormal(string input)
-        {
-            input = Regex.Replace(input, @"\s+", " ");
-            input = Regex.Replace(input, @"-+", "-");
-            input = Regex.Replace(input, @" - ", "-");
-            input = Regex.Replace(input, @"- -", " ");
-            bool isSpacePressed = true;
-            foreach (var ch in input)
-            {
-                if (isSpacePressed)
-                {
-                    if (Char.IsLower(ch))
-                        return false;
-                    isSpacePressed = false;
-                }
-                else if (Char.IsUpper(ch))
-                    return false;
-                if (ch == ' ' || ch == '-')
-                    isSpacePressed = true;
-            }
-            return true;
-        }
+        }        
     }
-
 }
